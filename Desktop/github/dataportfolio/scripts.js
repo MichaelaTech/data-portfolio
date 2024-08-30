0f3131981890bfed2f532e66fc8fc841a88440a2
MichaelaTech <145231600+MichaelaTech@users.noreply.github.com>
2024-08-30 11:02:10 -0600

    add all the files

---
+++ Desktop/github/dataportfolio/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! I will get back to you soon.');
+    // You can add more functionality here, such as sending the form data to your email.
+});
